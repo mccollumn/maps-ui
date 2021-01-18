d8323f5a5f1453c85462e59ef3800a57dd887814
Dominic <scientist1882@gmail.com>
2021-01-17 18:04:15 -0800

    User state example

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,14 +1,40 @@
 import React from "react";
 
 export const Users = ({children}) => {
+  // useState Example
+  const [counter, setCounter] = React.useState(0);
+
+  console.log('Render', counter);
+
+  const clickHandler = () => {
+    console.log('Am Clicked', counter);
+    //counter = counter +1;
+    setCounter(counter +1);
+  };
+
+  React.useEffect(() => {
+    console.log('After Draw', counter);
+  });
+
   return (
     <div>
       I am USERS ddddd
+      <div>
+        Count: {counter}
+      </div>
+
       <div
         style={{
           color: 'red'
         }}>
+
+
+        <hr/>
         {children}
+        <br/>
+        <button onClick={clickHandler}>
+          Hello
+        </button>
       </div>
     </div>
   );
