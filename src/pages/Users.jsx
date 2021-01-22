d195029daf93f8c5b26c4e97eeb0541cdb68c1c2
Nick McCollum <nmccollum81@gmail.com>
2021-01-21 18:13:34 -0800

    User CSS

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import useAxios from 'axios-hooks';
 
 export const Users = () => {
-  const [{ data = {data: []}, loading, error }, refetch] = useAxios(
+  const [{ data = { data: [] }, loading, error }, refetch] = useAxios(
     'https://reqres.in/api/users?delay=1'
   )
 
@@ -11,26 +11,53 @@ export const Users = () => {
   const UserList = data.data.map(User);
 
   return (
-    <div>
-      {UserList}
+    <div
+      className="users"
+      style={{
+        display: "flex",
+        justifyContent: "center"
+      }}>
+      <div
+        className="list"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          background: "aqua"
+        }}>
+        {UserList}
+      </div>
     </div>
   );
 };
 
-const User = (
-  {avatar,
+/* background: aqua;
+display: flex;
+flex-direction: column;
+align-items: flex-start; */
+
+const User = ({
+  avatar,
   email,
   first_name,
   last_name,
   id
-}
-) => {
+}) => {
   return (
-    <div key = {id}>
-      <img src={avatar}/>
-      <div>{first_name}</div>
-      <div>{last_name}</div>
-      <div>{email}</div>
+    <div
+      key={id}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+
+      <img src={avatar} />
+      <div style={{ margin: "10px" }}>
+        <div>{first_name}</div>
+        <div>{last_name}</div>
+        <div>{email}</div>
+      </div>
     </div>
   )
 }
