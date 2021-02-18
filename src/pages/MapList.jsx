20adc5f053b9fb4650685592438349949a73efe8
Nick McCollum <nmccollum81@gmail.com>
2021-02-17 18:25:50 -0800

    Post form data

--- src/pages/MapList.jsx
+++ src/pages/MapList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 import _ from "lodash";
 import { DataTable } from '../components/DataTable';
+import useAxios from 'axios-hooks';
 
 export const MapList = () => {
     const locations = [
@@ -40,8 +41,22 @@ export const MapList = () => {
 
 
 const MapForm = () => {
+    const [{ data = { data: [] }, loading, error }, executePost] = useAxios(
+        {
+            url: '/locationData',
+            method: 'post',
+            baseURL: 'http://localhost:3001',
+            headers: { 'Access-Control-Allow-Origin': '*' },
+        },
+        { manual: true }
+    );
     const { handleSubmit, register, errors } = useForm();
-    const onSubmit = values => console.log(values);
+    const onSubmit = values => {
+        console.log(values);
+        executePost({
+            data: values
+        });
+    }
     console.log(errors);
     let ratingValue = _.get(errors, "rating.ref.value");
     console.log(`${ratingValue} is not a valid rating`);
