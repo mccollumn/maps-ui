72813687fad0642f97ac7749591e36cb5d980a2e
Nick McCollum <nmccollum81@gmail.com>
2021-03-08 17:58:49 -0800

    Add login

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,8 +1,54 @@
 import * as Realm from "realm-web";
+import { useForm } from "react-hook-form";
 
 const REALM_APP_ID = process.env.REACT_APP_REALM_APP_ID;
 const app = new Realm.App({ id: REALM_APP_ID });
 
 export const Authentication = ({ children }) => {
+  if (!app.currentUser) {
+    return <Login />;
+  }
   return children;
 };
+
+export const Login = () => {
+  const { handleSubmit, register, errors } = useForm();
+  const onSubmit = async (values) => {
+    console.log(values);
+    const credentials = Realm.Credentials.emailPassword(
+      values.email,
+      values.password
+    );
+    try {
+      const user = await app.logIn(credentials);
+      console.log("User:", user);
+      window.location.reload();
+    } catch (err) {
+      console.log("Login Error:", err);
+    }
+  };
+
+  return (
+    <div className="login-page">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="email" name="email" ref={register} />
+        <input type="password" name="password" ref={register} />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+// async function loginEmailPassword(email, password) {
+//   // Create an anonymous credential
+//   const credentials = Realm.Credentials.emailPassword(email, password);
+//   try {
+//     // Authenticate the user
+//     const user = await app.logIn(credentials);
+//     // `App.currentUser` updates to match the logged in user
+//     assert(user.id === app.currentUser.id);
+//     return user;
+//   } catch (err) {
+//     console.error("Failed to log in", err);
+//   }
+// }
