f9e69f886f9e95e49349c727683499ffaad5cf2c
Nick McCollum <nmccollum@frontier.com>
2021-05-05 17:27:08 -0700

    Added map pin

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import GoogleMapReact from "google-map-react";
-import "./marker.css";
 
 const GOOGLE_MAP_API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
 
@@ -59,23 +58,37 @@ export const Map = ({ user }) => {
           streetViewControl: true,
         }}
       >
-        <Marker
-          lat={location.lat}
-          lng={location.long}
-          name={location.name}
-          color={"blue"}
-        />
+        <Marker lat={location.lat} lng={location.long} name={location.name} />
       </GoogleMapReact>
     </div>
   );
 };
 
-const Marker = ({ color, name }) => {
+const Marker = ({ name }) => {
   return (
-    <div
-      className="marker"
-      style={{ backgroundColor: color, cursor: "pointer" }}
-      title={name}
-    />
+    <div className="marker" title={name}>
+      <MapPin />
+    </div>
   );
 };
+
+function MapPin(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={21.333}
+      height={37.218}
+      viewBox="0 0 20 34.892"
+      {...props}
+    >
+      <g transform="matrix(1.18559 0 0 1.18559 -965.773 -331.784)">
+        <path
+          d="M817.112 282.971c-1.258 1.343-2.046 3.299-2.015 5.139.064 3.845 1.797 5.3 4.568 10.592.999 2.328 2.04 4.792 3.031 8.873.138.602.272 1.16.335 1.21.062.048.196-.513.334-1.115.99-4.081 2.033-6.543 3.031-8.871 2.771-5.292 4.504-6.748 4.568-10.592.031-1.84-.759-3.798-2.017-5.14-1.437-1.535-3.605-2.67-5.916-2.717-2.312-.048-4.481 1.087-5.919 2.621z"
+          fill="#ff4646"
+          stroke="#d73534"
+        />
+        <circle r={3.035} cy={288.253} cx={823.031} fill="#590000" />
+      </g>
+    </svg>
+  );
+}
